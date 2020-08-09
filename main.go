2b6043b0b1bb7dc7d16fec765c5635ece42afb2d
Awezome <likezyp@gmail.com>
2020-08-09 21:32:44 +0800

    use go sciter

--- main.go
+++ main.go
@@ -2,86 +2,55 @@ package main
 
 import (
 	"errors"
+	"fmt"
 	"gboard_dict/dict"
 	"io/ioutil"
+	"log"
 	"os"
 
 	"github.com/mholt/archiver/v3"
-	"github.com/therecipe/qt/core"
-	"github.com/therecipe/qt/widgets"
+	"github.com/sciter-sdk/go-sciter"
+	"github.com/sciter-sdk/go-sciter/window"
 )
 
-type CustomLabel struct {
-	widgets.QLabel
-	_ func()       `constructor:"init"`
-	_ func(string) `signal:"updateTextFromGoroutine"`
-}
-
-func (c *CustomLabel) init() {
-	c.ConnectUpdateTextFromGoroutine(c.SetText)
-}
-
-var label *CustomLabel
-
-// type mainThreadHelper struct {
-// 	core.QObject
-// 	_ func(f func()) `signal:"runOnMain,auto`
-// }
-//func (*mainThreadHelper) runOnMain(f func()) { f() }
-//var MainThreadRunner = NewMainThreadHelper(nil)
+var dom *sciter.Element
+var domMessage *sciter.Element
 
 func main() {
-	app := widgets.NewQApplication(len(os.Args), os.Args)
-	window := widgets.NewQMainWindow(nil, 0)
-	window.SetFixedWidth(350)
-	window.SetFixedHeight(160)
-	window.SetWindowTitle("搜狗词库转Gboard词库工具")
+	w, err := window.New(sciter.SW_TITLEBAR|sciter.SW_RESIZEABLE|sciter.SW_CONTROLS|sciter.SW_MAIN|sciter.SW_ENABLE_DEBUG, nil)
+	if err != nil {
+		log.Fatal("Create Window Error: ", err)
+	}
+	w.LoadFile("index.html")
 
-	title := widgets.NewQLabel2("搜狗词库转Gboard词库工具V2.0", nil, 0)
-	title.SetAlignment(core.Qt__AlignCenter)
-	title.SetStyleSheet("font-size:18px;")
-	title.SetFixedHeight(50)
+	dom, _ = w.GetRootElement()
+	domMessage, _ = dom.SelectFirst("#message")
 
-	info := widgets.NewQLabel2("<a href='https://github.com/Awezome/sougou_dict_to_gboard'>使用说明</a>  <a href='https://github.com/Awezome/sougou_dict_to_gboard/releases'>检查更新</a>", nil, 0)
-	info.SetAlignment(core.Qt__AlignLeft | core.Qt__AlignBottom)
-	info.SetOpenExternalLinks(true)
+	setEventHandler(w)
+	w.Show()
+	w.Run()
+}
 
-	input := widgets.NewQLineEdit(nil)
-	input.SetPlaceholderText("请输入搜狗词库网址...")
-	input.SetText("https://pinyin.sogou.com/dict/detail/index/4")
+func setEventHandler(w *window.Window) {
 
-	label = NewCustomLabel(nil, 0)
-	label.SetAlignment(core.Qt__AlignLeft)
+	w.DefineFunction("getNetInformation", func(args ...*sciter.Value) *sciter.Value {
+		url := args[0].String()
+		fmt.Println(url)
 
-	button := widgets.NewQPushButton2("Start", nil)
-	button.ConnectClicked(func(bool) {
-		button.SetDisabled(true)
 		go func() {
-			err := worker(input.Text())
+			err := worker(url)
 			if err != nil {
-				label.UpdateTextFromGoroutine(err.Error())
+				writeMessage(err.Error())
 			}
-			button.SetDisabled(false)
 		}()
+
+		return sciter.NullValue()
 	})
-
-	layout := widgets.NewQGridLayout2()
-	layout.AddWidget3(title, 0, 0, 1, 2, 0)
-	layout.AddWidget3(input, 1, 0, 1, 2, 0)
-	layout.AddWidget2(label, 2, 0, 0)
-	layout.AddWidget2(button, 2, 1, 0)
-	layout.AddWidget2(info, 3, 0, 0)
-
-	widget := widgets.NewQWidget(window, 0)
-	widget.SetLayout(layout)
-	window.SetCentralWidget(widget)
-	window.Show()
-	app.Exec()
 }
 
 func worker(url string) error {
 	var err error
-	label.UpdateTextFromGoroutine("开始加载...")
+	writeMessage("开始加载...")
 	d := &dict.Downloader{}
 	url, err = d.HtmlParser(url)
 	if err != nil {
@@ -90,21 +59,20 @@ func worker(url string) error {
 	if url == "" {
 		return errors.New("url is empty")
 	}
-	label.UpdateTextFromGoroutine("解析词库下载链接...")
-
+	writeMessage("下载...")
 	bytes, err := d.GetBytes(url)
 	if err != nil {
 		return err
 	}
 
-	label.UpdateTextFromGoroutine("转换中...")
+	writeMessage("读取...")
 
 	s := dict.SougouParser{}
 	err = s.Parse(bytes)
 	if err != nil {
 		return err
 	}
-
+	writeMessage("生成词库文本...")
 	txtPath := "./" + s.DictName + ".txt"
 	zipPath := "./" + s.DictName + ".zip"
 	content := s.FormatToImport()
@@ -112,13 +80,17 @@ func worker(url string) error {
 	if err != nil {
 		return err
 	}
-
+	writeMessage("开始打包...")
 	os.Remove(zipPath)
 	err = archiver.Archive([]string{txtPath}, zipPath)
 	os.Remove(txtPath)
 	if err != nil {
 		return err
 	}
-	label.UpdateTextFromGoroutine("转换完成")
+	writeMessage("完成")
 	return nil
+}
+
+func writeMessage(m string) {
+	domMessage.SetHtml(m, sciter.SIH_REPLACE_CONTENT)
 }
